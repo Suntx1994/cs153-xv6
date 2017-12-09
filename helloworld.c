eacb96589896f08bfddf35db39303a2f70b1e5a1
Sun <tianxiang.sun@hotmail.com>
2017-12-08 21:31:52 -0800

    init

---
+++ helloworld.c
@@ -0,0 +1,10 @@
+#include "types.h"
+//#include "stat.h"
+#include "user.h"
+int main(void)
+{
+	printf(0,"hello world (to fd 0)\n");
+	printf(1,"hello world (to fd 1)\n");
+//	exit(0);
+}
+//stat.h is not needed in this case
---
+++ sysproc.c
@@ -0,0 +1,115 @@
+#include "types.h"
+#include "x86.h"
+#include "defs.h"
+#include "date.h"
+#include "param.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "proc.h"
+
+int
+sys_fork(void)
+{
+  return fork();
+}
+
+int
+sys_exit(void)
+{
+  int status;
+  argint(0,&status);  // argint is used for dynamic allocation of heap
+  exit(status);
+  return 0;  // not reached
+}
+
+int
+sys_wait(void)
+{ 
+  int* status;
+  argptr(0,(char **) &status, sizeof(int*));  //argptr is used for dynamic allocation of heap
+  return wait(status);
+}
+
+int
+sys_waitpid(void)
+{
+   int pid;
+   int* status;
+   int options;
+   argint(0,&pid);
+   argptr(1,(char **) &status,sizeof(int*));  // argptr is used for dynamic allocation of heap
+   argint(2,&options);
+   return waitpid(pid,status,options);
+//   return 0;
+}
+int 
+sys_setpriority(void)
+{
+   int priority;
+   argint(0,&priority);      //argint is used for dynamic allocation of heap
+   setpriority(priority);
+   return priority;
+}
+int
+sys_kill(void)
+{
+  int pid;
+
+  if(argint(0, &pid) < 0)
+    return -1;
+  return kill(pid);
+}
+
+int
+sys_getpid(void)
+{
+  return myproc()->pid;
+}
+
+int
+sys_sbrk(void)
+{
+  int addr;
+  int n;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  addr = myproc()->sz;
+  if(growproc(n) < 0)
+    return -1;
+  return addr;
+}
+
+int
+sys_sleep(void)
+{
+  int n;
+  uint ticks0;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  acquire(&tickslock);
+  ticks0 = ticks;
+  while(ticks - ticks0 < n){
+    if(myproc()->killed){
+      release(&tickslock);
+      return -1;
+    }
+    sleep(&ticks, &tickslock);
+  }
+  release(&tickslock);
+  return 0;
+}
+
+// return how many clock tick interrupts have occurred
+// since start.
+int
+sys_uptime(void)
+{
+  uint xticks;
+
+  acquire(&tickslock);
+  xticks = ticks;
+  release(&tickslock);
+  return xticks;
+}
